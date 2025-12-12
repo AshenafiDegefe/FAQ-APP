191bb69ed54c4024aa9a0601809a2c315cd95995
AshenafiDegefe <ashenafidegefe8@gmail.com>
2025-12-12 11:31:28 +0300

    add FAQ Data

---
+++ faq-app/src/data/FAQData.js
@@ -0,0 +1,28 @@
+const faqData = [
+    {
+        id: 1,
+        question: 'What is React?',
+        answer: 'It is a JavaScript library for building user interfaces (UIs). It uses a declarative approach to create fast, dynamic web applications (SPAs) based on reusable components.'
+
+    },
+    {
+        id: 2,
+        question: 'What are Components?',
+        answer: 'They are the reusable building blocks of a React UI (e.g., a button, a form, a header). They isolate the UI logic and structure into self-contained pieces.'
+    },
+    {
+        id: 3,
+        question: 'What is JSX?',
+        answer: 'JavaScript XML. Its a syntax extension that lets you write HTML-like code directly within your JavaScript. A transpiler converts it into standard JavaScript for the browser.'
+    },
+    {
+        id: 4,
+        question: 'What is the Virtual DOM?',
+        answer: "A lightweight copy of the browser's Real DOM. React uses it to quickly find and apply only the minimal necessary changes to the browser, making updates faster."
+    },
+    {
+        id: 5,
+        question: 'State vs. Props?',
+        answer: 'Props are data passed down from a parent component (immutable/read-only). State is data managed internally by a component (mutable/can be changed).'
+    }
+]
